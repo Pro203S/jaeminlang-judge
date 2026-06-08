d9e85ab5d3f0d168baa9f1e55c72dc571b532596
Pro203S <prosirea2501@gmail.com>
2026-06-09 04:42:16 +0900

    chore: 파일 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "jaeminlang-judge",
-    description: "jaeminlang-judge",
+    title: "jaeminlang judge",
+    description: "재민랭을 온라인에서 풀어보세요!",
 };
 
 export default function RootLayout({
