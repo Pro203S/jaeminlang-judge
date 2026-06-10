bfd3174d55106157cd17bd035f64ed8451a93fbd
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-10 16:54:43 +0900

    chore: 페이지 추가

---
+++ src/app/problems/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "@/components/header";
+import css from './page.module.css';
+
+export default function Page() {
+
+    return <>
+        <Header />
+        <div className={css.container}>
+
+        </div>
+    </>;
+}
