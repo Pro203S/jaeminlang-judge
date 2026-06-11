2036d81dc3edbae0ea3fc89aa43006c71eabf5a4
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-11 16:22:52 +0900

    chore: Button에 prop 추가

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,31 +1,35 @@
 import Link from "next/link";
-import { CSSProperties } from "react";
+import { CSSProperties, ReactNode, RefObject } from "react";
 import css from './styles.module.css';
 
 type Props = {
-    "label": string,
+    "label"?: string,
     "href"?: string,
     "onClick"?: () => any,
     "className"?: string,
     "containerStyle"?: CSSProperties
+    "children"?: ReactNode
+    "ref"?: RefObject<HTMLButtonElement | null> | RefObject<HTMLAnchorElement | null>;
 }
 
 export default function Button(props: Props) {
-    const { label, href, onClick, className, containerStyle } = props;
+    const { label, href, onClick, className, containerStyle, children, ref } = props;
 
     if (href) return <Link
+        ref={ref as RefObject<HTMLAnchorElement | null>}
         href={href}
         style={containerStyle}
         className={`${css.button} ${className}`}
     >
-        <span>{label}</span>
+        {children ?? <span>{label}</span>}
     </Link>;
 
     return <button
+        ref={ref as RefObject<HTMLButtonElement | null>}
         onClick={onClick}
         style={containerStyle}
         className={`${css.button} ${className}`}
     >
-        <span>{label}</span>
+        {children ?? <span>{label}</span>}
     </button>
 }
