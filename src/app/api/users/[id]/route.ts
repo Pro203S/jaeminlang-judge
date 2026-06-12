6b356b83301fa8ac45ad8f28ce3c6cf693dc587d
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-12 20:23:40 +0900

    chore: 유저 정보 가져오는 루트

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,35 @@
+import { APIErrorResponse } from "@/modules/apiError";
+import { getDatabase } from "@/modules/database";
+import { MakeApiUser } from "@/modules/makeApiType";
+import { Pro203SSessionError } from "@/modules/pro203sAuth";
+import { NextRequest, NextResponse } from "next/server";
+
+type Params = { "params": Promise<{ id: string }> };
+
+export async function GET(req: NextRequest, { params }: Params) {
+    try {
+        const { id } = await params;
+        const database = getDatabase();
+        const users = database.get("users").value().sort((a, b) => b.score - a.score);
+        const found = users.find(v => v.id === id);
+        if (!found) return NextResponse.json({
+            "code": "not_found",
+            "message": "유저를 찾을 수 없습니다."
+        }, { "status": 404 });
+
+        return NextResponse.json(MakeApiUser(found));
+    } catch (err) {
+        if (err instanceof Pro203SSessionError) {
+            return NextResponse.json({
+                "code": err.status === 401 ? "unauthorized" : err.detail.code,
+                "message": err.detail.message
+            } satisfies APIErrorResponse, { "status": err.status });
+        }
+
+        const e = err as Error;
+        return NextResponse.json({
+            "code": e.name,
+            "message": e.message
+        } satisfies APIErrorResponse, { "status": 500 });
+    }
+}
